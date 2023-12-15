c7197d04d6b5d587d306d88a495e583f2e3dc8fa
Yevheniia2401 <yevhenia2401@gmail.com>
2023-12-15 18:00:20 +0200

    HW 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict' ;
+const userFirstNumber = Number(prompt('Your first number'));
+const userSecondNumber = Number(prompt('Your second number'));
+const add = userSecondNumber + userSecondNumber;
+const sub = userFirstNumber - userSecondNumber;
+const multi = userFirstNumber * userSecondNumber;
+const div = userFirstNumber / userSecondNumber;
+
+console.log(`${userFirstNumber}+${userSecondNumber}=${add}`);
+console.log(`${userFirstNumber}-${userSecondNumber}=${sub}`);
+console.log(`${userFirstNumber}*${userSecondNumber}=${multi}`);
+console.log(`${userFirstNumber}/${userSecondNumber}=${div}`);
