03538a5f3ed4d3a6baa74910ad852ea57a5fe2c5
Yevheniia2401 <yevhenia2401@gmail.com>
2023-12-15 17:25:51 +0200

    HW 2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict' ;
+const name = 'John';
+let admin;
+admin = name;
+alert(admin)
+
+let planetEarth;
+let siteVisitorName;
+
+const variableType = '10' + 10;
+console.log(variableType)
+console.log(typeof variableType)
