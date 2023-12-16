1ae3e34f0e500cddcdfa54520496b8b66a1117a5
Yevheniia2401 <yevhenia2401@gmail.com>
2023-12-16 21:25:19 +0200

    change script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,11 @@
 const name = 'John';
 let admin;
 admin = name;
-alert(admin)
+alert(admin);
 
 let planetEarth;
 let siteVisitorName;
 
 const variableType = '10' + 10;
-console.log(variableType)
-console.log(typeof variableType)
+console.log(variableType);
+console.log(typeof variableType);
