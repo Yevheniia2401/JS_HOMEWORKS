7ace897b32614b6257f2db9799f73223159f6fc1
Yevheniia2401 <yevhenia2401@gmail.com>
2023-12-25 21:49:51 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 'use strict' ;
+
+const userString1 = prompt('Your first message');
+const userString2 = prompt('Your second message');
+const userString3 = prompt('Your third message');
+
+alert(userString1 + ' ' + userString2 + ' ' + userString3);
+alert(3 + ' ' + 4 + ' ' + 8 + ' ' + 6 + ' '+ 2 );
