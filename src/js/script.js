4226c07fbdd537aac82f3bdd2033425864b2ea5c
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-02 17:56:31 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 'use strict' ;
 
 const action = prompt('What action do you want to perform? (add, sub, mult or div)');
-const num1 = Number(prompt('Your first number'));
-const num2 = Number(prompt('Your second number'));
+const number1 = Number(prompt('Your first number'));
+const number2 = Number(prompt('Your second number'));
 
 if (action === 'add') {
-    alert(num1 + '+' + num2 + '=' + (num1+num2))
+    alert(number1 + '+' + number2 + '=' + (number1+number2))
 }
 if (action === 'sub') {
-    alert(num1 + '-' + num2 + '=' + (num1-num2))
+    alert(number1 + '-' + number2 + '=' + (number1-number2))
 }
 if (action === 'mult') {
-    alert(num1 + '*' + num2 + '=' + (num1*num2))
+    alert(number1 + '*' + number2 + '=' + (number1*number2))
 }
 if (action === 'div') {
-    alert(num1 + '/' + num2 + '=' + (num1/num2))
+    alert(number1 + '/' + number2 + '=' + (number1/number2))
 }
