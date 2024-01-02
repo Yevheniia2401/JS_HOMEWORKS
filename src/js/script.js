49df00665776101f7e76ee9dbf9ee21687305ad6
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-02 17:18:05 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 'use strict' ;
+
+const number1 = Number(prompt());
+const number2 = Number(prompt());
+const number3 = Number(prompt());
+const sum = number1 + number2 + number3;
+alert('Arithmetic mean' +  ' = ' + sum/3)
