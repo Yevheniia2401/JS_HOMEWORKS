fb72c31dcbaef2d25f08f4b19f5159c65cc42dea
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-02 17:54:32 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,9 @@
 'use strict' ;
 
+const number1 = Number(prompt())
+const number2 = Number(prompt())
+
+alert(number1 + "+" + number2 + "=" + (number1+number2) + "\n" +
+    number1 + "-" + number2 + "=" + (number1-number2) + "\n" +
+    number1 + "*" + number2 + "=" + (number1*number2) + "\n" +
+    number1 + "/" + number2 + "=" +(number1/number2))
