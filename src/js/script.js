7604dd0fd964c83d505d164298128e5f43e2536f
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-07 15:36:07 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
 'use strict' ;
 
+const arr = [1, 2, 3, 4, 5]
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum)
+
+for (let i = 0; i < arr.length; i++) {
+    sum+= Math.pow(arr[i],2)
+}
+console.log(sum)
+
