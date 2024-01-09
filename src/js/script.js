e08e4052b4d3cbb4fd0078a2e934b0b03d4d3e97
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-09 15:24:45 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,12 @@
 'use strict' ;
 
+let sum = 0;
+outerLoop: for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        sum += i;
+    }else {
+        break outerLoop;
+    }
+}
+
+alert(sum);
