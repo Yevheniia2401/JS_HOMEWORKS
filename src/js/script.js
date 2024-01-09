40dc90191edf143f8bbe95bd3b8f5b9ed23e9e86
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-09 14:02:58 +0200

    complete homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,22 @@
 'use strict' ;
+let result = 0;
+const number = +prompt('Enter your number');
+if (number < 1) {
+    console.log(NaN)
+}else {
+     for (let i = 2; i < number - 1; i++) {
+         if (number % i === 0) {
+            result = i; break
+            }
+        }
+    }
+    if (result === 0) {
+        console.log('Просте число')
+    }else {
+        console.log(result)
+    }
+
+
+
+
 
