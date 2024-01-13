2aefc25a417291a577d7ef9da15a5788677bd4a6
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-13 15:35:48 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 'use strict' ;
+const userNumber = +prompt('Введіть число');
+const powOfNumber = +prompt('Введіть ступінь числа');
+function resultNumber (userNumber, powOfNumber = 1) {
+    if (isNaN(userNumber) || isNaN(powOfNumber)) {
+        return 'Помилка'
+    }else
+        return  Math.pow(userNumber, powOfNumber)
+}
+const result = resultNumber(userNumber, powOfNumber);
+alert(result)
+
 
