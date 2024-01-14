afefbe318ddd35c5d349200f1f9f436712b4dfcf
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-14 03:53:44 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict' ;
+const arr = [1, 2, 3, -1, -2, -3];
+function currentPositiveNumber (arr) {
+    let exampleArr = [];
+    if ((arr.length === 0)) {
+        return console.log('Порожній масив');
+    }
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] > 0) {
+            exampleArr.push(arr[i])
+        }
+    }
+    console.log(exampleArr)
+
+    if (exampleArr.length !== 0) {
+        return exampleArr;
+    }else
+        return null;
+}
+currentPositiveNumber(arr);
+
+
