dfbd19c99a6f76bd98a2f2a958b80f3b00b3b95d
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-15 19:52:43 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict' ;
+
+const userNumber = prompt('Enter your number');
+    const number = +userNumber;
+    if (number % 10 === 1 && (number / 10 ) % 10 !== 1) {
+          console.log(number + ' рік')
+    }
+    if ((number % 10 === 2 || number % 10 === 3 || number % 10 === 4) && ((number / 10 ) % 10 !== 1)) {
+         console.log(number + ' роки')
+    }else {
+        console.log(number + ' років')
+    }
