746fac9718a04df8b839df90299b8a7695fa697c
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-20 22:33:36 +0200

    added arguments to the function

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 const weightOfPotatoes = 0.075;
 const numberOfLetters = 48;
 const priceOfKilogram = 13;
-const priceOfPotatoes = function () {
+const priceOfPotatoes = function (weightOfPotatoes, numberOfLetters, priceOfKilogram) {
     return Math.ceil((4*weightOfPotatoes) * numberOfLetters) * priceOfKilogram;
 }
 console.log(priceOfPotatoes(weightOfPotatoes, numberOfLetters, priceOfKilogram));
