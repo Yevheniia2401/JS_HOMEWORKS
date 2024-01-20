943f3401d53fc03aa716633a639c7c263679afce
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-20 23:03:23 +0200

    code correction

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,24 @@
 'use strict' ;
 const arr = [1, 2, 3, -1, -2, -3];
 
-if (Array.isArray (arr)) {
-    function currentPositiveNumber(arr) {
+
+function currentPositiveNumber(arr) {
+    if (Array.isArray(arr)) {
         const exampleArr = [];
         if ((arr.length === 0)) return console.log('Порожній масив');
 
         for (let i = 0; i <= arr.length; i++) {
             if (arr[i] > 0) exampleArr.push(arr[i])
         }
-        console.log(exampleArr)
+        if (exampleArr.length !== 0) return exampleArr
+            return null;
+    } else console.log('Error')
+}
+const result = currentPositiveNumber(arr);
+console.log(result)
 
-        if (exampleArr.length !== 0) return exampleArr;
-        return null;
-    }
-    currentPositiveNumber(arr)
-}else console.log('Error')
+
+
 
 
 
