68dce67426b9b2863bed5ac766a46679a9b9f511
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-21 14:58:52 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,16 @@
 'use strict' ;
 
 const userNumber = prompt('Enter your number');
+if (typeof userNumber !== "number" ) {
+    throw new Error(`The transferred value is not a number`);
+}else {
     const number = +userNumber;
-    if (number % 10 === 1 && (number / 10 ) % 10 !== 1) {
-          console.log(number + ' рік')
+    if (number % 10 === 1 && (number / 10) % 10 !== 1) {
+        console.log(number + ' рік')
     }
-    if ((number % 10 === 2 || number % 10 === 3 || number % 10 === 4) && ((number / 10 ) % 10 !== 1)) {
-         console.log(number + ' роки')
-    }else {
+    if ((number % 10 === 2 || number % 10 === 3 || number % 10 === 4) && ((number / 10) % 10 !== 1)) {
+        console.log(number + ' роки')
+    } else {
         console.log(number + ' років')
     }
+}
