39d8b4937abcb49431282c1e08c07bd287d1b22e
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-22 21:59:37 +0200

    completed homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-'use strict' ;
+const User = function (name, age) {
+  this.name = name
+  this.age = age
+}
+User.prototype.greet = function () {
+  console.log('Hello ' + this.name + ' Your age is ' + this.age)
+}
+const user1 = new User('Anna', 16)
+const user2 = new User('Bob', 27)
+const user3 = new User('Mike', 45)
+user1.greet()
+user2.greet()
+user3.greet()
