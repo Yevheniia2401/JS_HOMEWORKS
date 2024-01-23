757b99deb4c198ad7ebc1e7d014d45ddf18f275a
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-23 17:15:23 +0200

    change homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-'use strict' ;
+const usedNumbers = []
+const randomNumber = function () {
+  const randomNumResult = Math.floor(Math.random() * 100)
+  if (!usedNumbers.includes(randomNumResult)) {
+    usedNumbers.push(randomNumResult)
+    return randomNumResult
+  }
+  return randomNumber()
+}
+for (let i = 0; i <= 99; i += 1) {
+  console.log(randomNumber())
+}
