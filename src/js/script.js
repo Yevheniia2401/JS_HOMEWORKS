bcab6ebcd0c0368798893a2ed5035425e1c0bd37
Yevheniia2401 <yevhenia2401@gmail.com>
2024-02-02 20:02:46 +0200

    change homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,61 +1,59 @@
 // // task 1//
-const CreateCandidate = function NewCand(name, skills) {
+
+const CreateCandidate = function (name, skills = []) {
   return {
-    nameOfCandidate: name,
-    skillsOfCandidate: skills,
+    name,
+    skills,
   };
 };
+
 const candidate1 = new CreateCandidate('Anna', ['HTML', 'CSS', 'SCSS']);
 const candidate2 = new CreateCandidate('Mike', ['HTML', 'CSS', 'SCSS', 'Bootstrap', 'JS', 'React']);
 const candidate3 = new CreateCandidate('Bob', ['HTML', 'CSS', 'SCSS', 'Bootstrap']);
 console.log(candidate1, candidate2, candidate3);
 // task 2//
-const AddRequirement = function NewReq() {
+const AddRequirement = function () {
   const requirementList = ['HTML', 'CSS', 'SCSS', 'Bootstrap', 'JS'];
   requirementList.push('React');
   return {
-    requirement: requirementList,
-    length: requirementList.length,
+    requirementList,
   };
 };
 const requirementOfCandidate = AddRequirement();
 
 // task 3//
-// const conductAnInterview = function (candidate2, requirementOfCandidate) {
-//   for (let i = 0; i < requirementOfCandidate.length; i++) {
-//     if (candidate2.skillsOfCandidate.includes(requirementOfCandidate[i])) {
-//       return true
-//     } else return false
-//   }
-// }
+const conductAnInterview = function (candidate, requirement) {
+  let result = 100;
 
-// const conductAnInterview = function (candidate2, requirementOfCandidate) {
-//   if (candidate2.skillsOfCandidate.length !== requirementOfCandidate.length) {
-//     return false
-//   } return true
-// }
-
-// eslint-disable-next-line no-shadow
-const conductAnInterview = function NewInter(candidate2, requirementOfCandidate) {
-  for (let i = 0; i < requirementOfCandidate.length; i + 1) {
-    if (candidate2.skillsOfCandidate[i] !== requirementOfCandidate.requirement[i]) {
-      return false;
+  for (let r = 0; r < requirement.length; r += 1) {
+    let skillFound = false;
+    for (let i = 0; i < candidate.skills.length; i += 1) {
+      if (requirement[r] === candidate.skills[i]) {
+        skillFound = true;
+        break;
+      }
     }
-  } return true;
+    if (!skillFound) {
+      result = 0;
+    }
+  }
+  return result;
 };
-const resultOfInterview = conductAnInterview(candidate2, requirementOfCandidate);
+
+const resultOfInterview = conductAnInterview(candidate1, requirementOfCandidate);
 console.log(resultOfInterview);
+
 // task 4//
-const ratingOfCandidates = function NewRat() {
-  const numbers = [];
-  for (let i = 0; i <= 100; i + 1) {
-    numbers.push(i);
-  } return numbers;
+
+const planInterviews = function (candidates, req, scoreFunc) {
+  const result = [];
+  for (let i = 0; i < candidates.length; i += 1) {
+    result.push({
+      score: scoreFunc(candidates[i], req),
+      candidate: candidates[i],
+    });
+  }
+  return result;
 };
-const rating = ratingOfCandidates();
-console.log(rating);
-const interviewPlanning = function NewPlan() {
-  return [candidate1, candidate2, candidate3];
-};
-const result = interviewPlanning();
-console.log(result);
+const candidates = [candidate1, candidate2, candidate3];
+console.log(planInterviews(candidates, requirementOfCandidate, conductAnInterview));
