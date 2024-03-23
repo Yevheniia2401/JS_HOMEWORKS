20f7cb75e0902447e3a3c10637d8e271ffe79a5a
Yevheniia2401 <yevhenia2401@gmail.com>
2024-03-23 23:46:09 +0200

    homework is changed

--- js_course_3_hw_40-master/js_course_3_hw_40-master/js/script.js
+++ js_course_3_hw_40-master/js_course_3_hw_40-master/js/script.js
@@ -1,25 +1,41 @@
+const DATA_KEY = 'userData';
 (function () {
-  const userForm = document.querySelector('#form');
-  const resultForm = document.querySelector('[data-result-form]');
-  const dataKey = 'userData';
-  userForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const userInputs = event.target.querySelectorAll('input, select, textarea');
-    const userData = Array.from(userInputs).reduce((acc, item) => {
-      acc[item.name] = item.value;
-      return acc;
-    }, {});
-    localStorage.setItem(dataKey, JSON.stringify(userData));
-  });
-  document.addEventListener('DOMContentLoaded', () => {
-    const getData = JSON.parse(localStorage.getItem(dataKey));
-    if (!getData) return;
+  const formPage = () => {
+    const userForm = document.querySelector('#form');
+    userForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const userInputs = event.target.querySelectorAll('input, select, textarea');
+      const data = {};
+      userInputs.forEach((input) => {
+        if (input.value.trim() !== '') {
+          data[input.name] = input.value;
+        }
+      });
+      if (Object.keys(data).length === 0) {
+        alert('You should fill the form');
+        return;
+      }
+      localStorage.setItem(DATA_KEY, JSON.stringify(data));
+    });
+  };
+  const outputPage = () => {
+    const data = JSON.parse(localStorage.getItem(DATA_KEY));
+    const main = document.querySelector('main');
+    if (!data) {
+      main.innerHTML = '<h1>No Data</h1>';
+      return;
+    }
     const ul = document.createElement('ul');
-    for (const key in getData) {
+    for (const key in data) {
       const li = document.createElement('li');
-      li.innerHTML = `<b>${key}:</b> <i>${getData[key]}</i>`;
+      li.innerHTML = `<b>${key}:</b> ${data[key]}`;
       ul.append(li);
     }
-    resultForm.append(ul);
-  });
+    main.append(ul);
+  };
+  if (location.pathname.includes('index.html')) {
+    formPage();
+  } else if (location.pathname.includes('second.html')) {
+    outputPage();
+  }
 }());
